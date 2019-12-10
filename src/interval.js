3a0711e36da273dd58a7785d5b0029370f3879d5
Adrien David-Sivelle <adrien.ooo@gmail.com>
2019-12-10 16:26:33 -0500

    Initial commit with union and subtract exported

---
+++ src/interval.js
@@ -0,0 +1,62 @@
+const { sortByStart, merge, split, removeElement, valid } = require('./utils')
+
+/**
+ * @param {[Interval]} iList
+ * @return {[Interval]} Start-ordered union of all intervals of iList
+ */
+function union (iList) {
+  if (iList.length <= 1) return iList
+  sortByStart(iList)
+  const ret = []
+  for (const interval of iList) {
+    if (!valid(interval)) continue
+    try {
+      ret[ret.length - 1] = merge(ret[ret.length - 1], interval)
+    } catch (e) {
+      ret.push(interval)
+    }
+  }
+  return ret
+}
+
+/**
+ * @param {[Interval]} iListA
+ * @param {[Interval]} iListB
+ * @return {[Interval]} Start-ordered union of all intervals of iListA - intervals of iListB
+ */
+function subtract (iListA, iListB) {
+  const setA = union(iListA)
+  const setB = union(iListB)
+  console.log(JSON.stringify(setA), '-', JSON.stringify(setB))
+
+  let iB = 0
+  for (let iA = 0; iA < setA.length; iA++) {
+    const [startA, endA] = setA[iA]
+    for (; iB < setB.length; iB++) {
+      const [startB, endB] = setB[iB]
+      if (startA >= endB) continue //       (B) [A]        : continue to next element of setB
+      if (endB < endA) {
+        if (startA > startB) {
+          setA[iA][0] = endB //             (B [ ) A]      : startA <- endB
+        } else {
+          split(setA, iA, startB, endB) //  [ (B)  A]      : split [A(, )A']
+          iA++
+        }
+      } else {
+        if (startB < startA) {
+          removeElement(setA, iA) //        (B [ A ] )     : remove A from setA
+          iA--
+        } else if (startB < endA) {
+          setA[iA][1] = startB //           [A  (B ] )     : endA <- startB
+        } //                          Else: [A] (B)        : Just ignore
+        break
+      }
+    }
+  }
+  return setA
+}
+
+module.exports = {
+  union,
+  subtract
+}
---
+++ src/utils.js
@@ -0,0 +1,111 @@
+/**
+ * @typedef {number|Date|*} Value
+ * @typedef {[Value, Value]} Interval
+ */
+
+/**
+ * @param {Interval} interval
+ * @return {Value}
+ */
+function start (interval) {
+  return interval[0]
+}
+
+/**
+ * @param {Interval} interval
+ * @return {Value}
+ */
+function end (interval) {
+  return interval[1]
+}
+
+/**
+ * @param {Interval} interval
+ * @return {boolean}
+ */
+function valid (interval) {
+  return interval.length === 2 && start(interval) < end(interval)
+}
+
+/**
+ * Sort iList in place using start element of each interval
+ * @param {[Interval]} iList
+ */
+function sortByStart (iList) {
+  iList.sort((a, b) => start(a) - start(b))
+}
+
+function copyInterval (interval) {
+  return [start(interval), end(interval)]
+}
+
+/**
+ * Merge overlapping intervals
+ * @param {Interval} a
+ * @param {Interval} b
+ * @return {Interval}
+ */
+function merge (a, b) {
+  if (start(a) > start(b)) { // invert if not in start orders
+    [a, b] = [b, a]
+  }
+  if (end(a) > end(b)) {
+    return a
+  }
+  if (end(a) < start(b)) {
+    throw new Error(`Can not merge distinct intervals [${a}] & [${b}]`)
+  }
+  return [start(a), end(b)]
+}
+
+/**
+ * Split iList at given position, replacing the end and start values around the split by the given startVal & endVal
+ * @param {[Interval]} iList
+ * @param {Number} index
+ * @param {Value} startVal
+ * @param {Value|null} endVal
+ */
+function split (iList, index, startVal, endVal = null) {
+  if (endVal == null) endVal = startVal
+
+  /* Checks */
+  checkIndexInRange(iList, index)
+  const element = iList[index]
+  if (start(element) > startVal || end(element) < endVal) {
+    throw new Error(`Can not split [${element}] with [${startVal}, ${endVal}]`)
+  }
+
+  /* Split */
+  iList.splice(index, 0, copyInterval(iList[index]))
+  iList[index][1] = startVal
+  iList[index + 1][0] = endVal
+}
+
+/**
+ * Remove element from list
+ * @param {[Interval]} iList
+ * @param {Number} index
+ */
+function removeElement (iList, index) {
+  checkIndexInRange(iList, index)
+  iList.splice(index, 1)
+}
+
+/**
+ * Throw an error if the index is not in the boundaries of the list iList
+ * @param {[Interval]} iList
+ * @param {Number} index
+ */
+function checkIndexInRange (iList, index) {
+  if (index < 0 || index >= iList.length) {
+    throw new Error(`Can not operate on element ${index} of set of length ${iList.length}`)
+  }
+}
+
+module.exports = {
+  sortByStart,
+  merge,
+  split,
+  removeElement,
+  valid
+}
